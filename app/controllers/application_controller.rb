e63788085b82bdf1004a1a6f3f77589f109fdbf5
Sara Daghustani <sara.daghustani@gmail.com>
2019-07-25 08:42:58 +0300

    desgin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def validate_each(record, attribute, value)
+        if value.match(PHONE_REGEX).blank?
+          record.errors.add(attribute, 'invalid phone number')
+        end
+      
+    end
 end
