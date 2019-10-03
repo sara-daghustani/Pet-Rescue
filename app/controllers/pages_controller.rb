262a47ef3beb0d4f9299a3e78e58b0819f83269c
Sara Daghustani <sara.daghustani@gmail.com>
2019-10-04 01:04:04 +0300

    search in home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ before_action :authenticate_user!, only: :private_page
 
   def home
     @reports = Report.all  
+    @reports = Report.where(["reporttype ILIKE ?","%#{params[:search]}%"])
+    @reports = Report.where(["city ILIKE ?","%#{params[:search]}%"])
   end
 
   def private_page
