ad717fe6bde38b443ec325552bb877c9345a675e
Sara Daghustani <sara.daghustani@gmail.com>
2019-10-04 01:46:03 +0300

    search with report type or city

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ before_action :authenticate_user!, only: :private_page
 
   def home
     @reports = Report.all  
-    @reports = Report.where(["reporttype ILIKE ?","%#{params[:search]}%"])
-    @reports = Report.where(["city ILIKE ?","%#{params[:search]}%"])
+    @reports = Report.where(
+      "reporttype ILIKE ? OR city ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
   end
 
   def private_page
