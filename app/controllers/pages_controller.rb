66517ea293ea95ed7df712f52a02a0821bc7b6aa
Sara Daghustani <sara.daghustani@gmail.com>
2019-10-04 12:29:11 +0300

    fixing search in home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
 before_action :authenticate_user!, only: :private_page
 
-  def home
-    @reports = Report.all  
-    @reports = Report.where(
-      "reporttype ILIKE ? OR city ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+
+
+  def home 
+    @reports = Report.all 
+    @reports = Report.where("reporttype ILIKE ? AND city ILIKE ? AND species ILIKE ?", "%#{params[:reporttype]}%", "%#{params[:city]}%", "%#{params[:species]}%")
+    
   end
 
   def private_page
