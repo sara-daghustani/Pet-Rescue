2325d916edd94cb871799e80881e4a0308145f6f
Sara Daghustani <sara.daghustani@gmail.com>
2019-10-19 16:51:28 +0300

    make comment by created order in comment model and in report show view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
     before_action :check_if_owner, only: [:edit, :update, :destroy]
     
 def index
-     
+    @comment = Post.paginate(page: params[:page], per_page: 15).order('created_at DESC')
+
 end
 def new
     @comment = Comment.new
